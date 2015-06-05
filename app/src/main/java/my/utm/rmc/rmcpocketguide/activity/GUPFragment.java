bfc80a1a9d59b30f732abd36a6f05bdc23236dc4
rahmatnaim <rahmatnaim@gmail.com>
2015-06-05 12:22:26 +0800

    base design

---
+++ app/src/main/java/my/utm/rmc/rmcpocketguide/activity/GUPFragment.java
@@ -0,0 +1,46 @@
+package my.utm.rmc.rmcpocketguide.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import my.utm.rmc.rmcpocketguide.R;
+
+/**
+ * Created by rahmatnaim on 6/5/15.
+ */
+public class GUPFragment extends Fragment {
+
+    public GUPFragment() {
+        //Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
